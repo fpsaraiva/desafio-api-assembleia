9c853bf07d3fa2f93efe6e6396890c49605c18e0
Fernando Saraiva <fpsaraiva@gmail.com>
2024-12-02 20:24:00 -0300

    feat: add arquivos projeto

---
+++ src/main/java/dev/fpsaraiva/api_assembleia/ApiAssembleiaApplication.java
@@ -0,0 +1,13 @@
+package dev.fpsaraiva.api_assembleia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAssembleiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAssembleiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fpsaraiva/api_assembleia/ApiAssembleiaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fpsaraiva.api_assembleia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAssembleiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
