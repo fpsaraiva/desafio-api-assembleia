9b4dd4856307f85635973eb1becbdb55982051c4
Fernando Saraiva <fpsaraiva@gmail.com>
2024-12-03 00:11:00 -0300

    feat: add excecao pauta sem titulo

--- src/main/java/dev/fpsaraiva/api_assembleia/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/fpsaraiva/api_assembleia/exception/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package dev.fpsaraiva.api_assembleia.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,5 +22,14 @@ public class GlobalExceptionHandler {
 
         ErroPadronizado erroPadronizado = new ErroPadronizado(mensagens);
         return ResponseEntity.status(apiErroException.getHttpStatus()).body(erroPadronizado);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
